92b8d3703d3b6c31bcf0711bd227c0a5ac736e81
Kayla Reopelle <kreopelle@gmail.com>
2018-07-25 13:49:19 +0000

    Create delete expense route, change button action

--- sinatra-expense-tracker/app/controllers/expenses_controller.rb
+++ sinatra-expense-tracker/app/controllers/expenses_controller.rb
@@ -30,14 +30,22 @@ class ExpensesController < ApplicationController
   end
 
   get '/expenses/:id' do
-    @expense = Expense.find(params[:id])
-    erb :'expenses/show_expense'
+    if logged_in?
+      @expense = Expense.find(params[:id])
+      erb :'expenses/show_expense'
+    else
+      redirect to('/login')
+    end
   end
 
   get '/expenses/:id/edit' do
-    @expense = Expense.find(params[:id])
-    @user = User.find(session[:user_id])
-    erb :'expenses/update_expense'
+    if logged_in?
+      @expense = Expense.find(params[:id])
+      @user = User.find(session[:user_id])
+      erb :'expenses/update_expense'
+    else
+      redirect to('/login')
+    end
   end
 
   patch '/expenses/:id' do
@@ -48,12 +56,21 @@ class ExpensesController < ApplicationController
     @expense.total = params[:total]
     @expense.payment_method = params[:payment_method]
     if !@expense.save
-      @errors = @expense.errors.full_messages #can't seem to have errors printed to console, but redirects properly? 
+      @errors = @expense.errors.full_messages #can't seem to have errors printed to console, but redirects properly?
       redirect to("/expenses/#{@expense.id}/edit")
     else
       redirect to("/expenses/#{@expense.id}")
     end
   end
 
+  delete '/expenses/:id/delete' do
+    if logged_in?
+      @expense = Expense.find(params[:id])
+      @expense.destroy
+      redirect to('/expenses')
+    else
+      redirect to('/login')
+    end
+  end 
 
 end
