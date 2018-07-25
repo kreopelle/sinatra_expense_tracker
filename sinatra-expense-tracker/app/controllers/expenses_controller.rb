2e08d0f9c2a71f96704eda433d7e31b99bcc1406
Kayla Reopelle <kreopelle@gmail.com>
2018-07-25 13:42:51 +0000

    Add edit function to expenses

--- sinatra-expense-tracker/app/controllers/expenses_controller.rb
+++ sinatra-expense-tracker/app/controllers/expenses_controller.rb
@@ -23,23 +23,35 @@ class ExpensesController < ApplicationController
     @expense = Expense.new(params)
     if !@expense.save
       @errors = @expense.errors.full_messages
-      erb :'expenses'
+      redirect to('/expenses')
     else
       redirect to('/expenses')
     end
   end
 
   get '/expenses/:id' do
-    
+    @expense = Expense.find(params[:id])
+    erb :'expenses/show_expense'
+  end
+
+  get '/expenses/:id/edit' do
+    @expense = Expense.find(params[:id])
+    @user = User.find(session[:user_id])
+    erb :'expenses/update_expense'
   end
 
   patch '/expenses/:id' do
-    @expense =
+    @expense = Expense.find(params[:id])
+    @expense.vendor = params[:vendor]
+    @expense.description = params[:description]
+    @expense.date = params[:date]
+    @expense.total = params[:total]
+    @expense.payment_method = params[:payment_method]
     if !@expense.save
-      @errors = @expense.errors.full_messages
-      erb :'expenses'
+      @errors = @expense.errors.full_messages #can't seem to have errors printed to console?
+      redirect to("/expenses/#{@expense.id}/edit")
     else
-      redirect to('/expenses')
+      redirect to("/expenses/#{@expense.id}")
     end
   end
 
--- sinatra-expense-tracker/app/models/expense.rb
+++ sinatra-expense-tracker/app/models/expense.rb
@@ -1,3 +1,5 @@
 class Expense < ActiveRecord::Base
   belongs_to :user
+  validates :vendor, :description, :date, :total, :payment_method, presence: true
+
 end
