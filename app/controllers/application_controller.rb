9360ad2ed69cc3bc0017b10ff36175f9ed16dda6
Kayla Reopelle <kreopelle@gmail.com>
2018-07-26 01:05:15 +0000

    Adjust session secret

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
     enable :sessions
-    set :session_secret, ENV.fetch('SESSION_SECRET')
+    set :session_secret, "bananas_in_pajamas"
   end
 
   get "/" do
